1f60ce7f6595dacdee0287321d4355becae97a12
yaroslavkuznietsov <yaroslav.kuznietsov@gmail.com>
2020-06-08 02:28:30 +0300

    Added ModelChoiceField to the PizzaForm

--- store/store/forms.py
+++ store/store/forms.py
@@ -3,6 +3,8 @@ from store.models import Size, Pizza
 
 
 class PizzaForm(forms.ModelForm):
+    pizz = forms.ModelChoiceField(queryset=Pizza.objects.all().order_by('title'), required=True, label=None,
+                                initial=Pizza.objects.all().order_by('title').first(), help_text='Choose your destiny')
     size = forms.ChoiceField(choices=Size.SIZE_OPTIONS)
     quantity = forms.IntegerField(widget=forms.NumberInput(), min_value=1, required=True, initial=1)
     address = forms.CharField(max_length=64)
@@ -12,3 +14,4 @@ class PizzaForm(forms.ModelForm):
     class Meta:
         model = Pizza
         fields = ['id', ]
+
